c492d5256fd0107c1c85c2f4eec0dcbf86e4e446
John Shea <jscruffy@hotmail.com>
2015-12-20 15:43:59 -0500

    Create GCE module

--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -1,14 +1,24 @@
 package com.udacity.gradle.builditbigger;
 
+import android.content.Context;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Pair;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
-import com.sheajohnh.JokeMaker;
+import com.google.api.client.extensions.android.http.AndroidHttp;
+import com.google.api.client.extensions.android.json.AndroidJsonFactory;
+import com.google.api.client.googleapis.services.AbstractGoogleClientRequest;
+import com.google.api.client.googleapis.services.GoogleClientRequestInitializer;
 import com.sheajohnh.android.jokedisplay.JokeDisplayActivity;
+import com.sheajohnh.myapplication.backend.myApi.MyApi;
+
+import java.io.IOException;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -45,15 +55,55 @@ public class MainActivity extends ActionBarActivity {
     public void tellJoke(View view){
 //        Toast.makeText(this, "derp", Toast.LENGTH_SHORT).show();
 
-        JokeMaker jokeMaker = new JokeMaker();
-        String currentJoke = jokeMaker.getJoke();
+//        JokeMaker jokeMaker = new JokeMaker();
+//        String currentJoke = jokeMaker.getJoke();
 
 //        Toast.makeText(this, currentJoke, Toast.LENGTH_SHORT).show();
 
-        Intent intent = new Intent(this, JokeDisplayActivity.class);
-        intent.putExtra("joke", currentJoke);
-        startActivity(intent);
+//        Intent intent = new Intent(this, JokeDisplayActivity.class);
+//        intent.putExtra("joke", currentJoke);
+//        startActivity(intent);
+
+        new EndpointsAsyncTask().execute(new Pair<Context, String>(this, "Manfred"));
     }
 
+}
 
+class EndpointsAsyncTask extends AsyncTask<Pair<Context, String>, Void, String> {
+    private static MyApi myApiService = null;
+    private Context context;
+
+    @Override
+    protected String doInBackground(Pair<Context, String>...params) {
+        if(myApiService == null) {
+            MyApi.Builder builder = new MyApi.Builder(AndroidHttp.newCompatibleTransport(),
+                    new AndroidJsonFactory(), null)
+                    .setRootUrl("http://10.0.3.2:8080/_ah/api")
+                    .setGoogleClientRequestInitializer(new GoogleClientRequestInitializer() {
+                        @Override
+                        public void initialize(AbstractGoogleClientRequest<?> abstractGoogleClientRequest) throws IOException {
+                            abstractGoogleClientRequest.setDisableGZipContent(true);
+                        }
+                    });
+            myApiService = builder.build();
+        }
+
+        context = params[0].first;
+        String name = params[0].second;
+
+        try {
+            return myApiService.sayHi(name).execute().getData();
+        } catch (IOException e) {
+            return e.getMessage();
+        }
+    }
+
+    @Override
+    protected void onPostExecute(String result) {
+        Toast.makeText(context, result, Toast.LENGTH_LONG).show();
+
+        Intent intent = new Intent(context, JokeDisplayActivity.class);
+        intent.putExtra("joke", result);
+        context.startActivity(intent);
+    }
 }
---
+++ backend/src/main/java/com/sheajohnh/myapplication/backend/MyEndpoint.java
@@ -0,0 +1,41 @@
+/*
+   For step-by-step instructions on connecting your Android application to this backend module,
+   see "App Engine Java Endpoints Module" template documentation at
+   https://github.com/GoogleCloudPlatform/gradle-appengine-templates/tree/master/HelloEndpoints
+*/
+
+package com.sheajohnh.myapplication.backend;
+
+import com.google.api.server.spi.config.Api;
+import com.google.api.server.spi.config.ApiMethod;
+import com.google.api.server.spi.config.ApiNamespace;
+import com.sheajohnh.JokeMaker;
+
+import javax.inject.Named;
+
+/** An endpoint class we are exposing */
+@Api(
+  name = "myApi",
+  version = "v1",
+  namespace = @ApiNamespace(
+    ownerDomain = "backend.myapplication.sheajohnh.com",
+    ownerName = "backend.myapplication.sheajohnh.com",
+    packagePath=""
+  )
+)
+public class MyEndpoint {
+
+    /** A simple endpoint method that takes a name and says Hi back */
+    @ApiMethod(name = "sayHi")
+    public MyBean sayHi(@Named("name") String name) {
+        MyBean response = new MyBean();
+//        response.setData("Hi, " + name);
+
+        JokeMaker jokeMaker = new JokeMaker();
+        String newJoke = jokeMaker.getJoke();
+        response.setData("Hi..." + newJoke);
+
+        return response;
+    }
+
+}
